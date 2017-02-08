7026d5ec4223070aa573c6cc3e83c8e7c4968587
saibot777 <stefantrajkovic777@gmail.com>
2017-02-09 00:18:14 +0100

    services, http, models created, back-end mock

---
+++ app/model/product.model.js
@@ -0,0 +1,15 @@
+"use strict";
+/**
+ * Created by stefan.trajkovic on 8.2.2017..
+ */
+var Product = (function () {
+    function Product(id, name, category, description, price) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.description = description;
+        this.price = price;
+    }
+    return Product;
+}());
+exports.Product = Product;
---
+++ app/model/product.model.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by stefan.trajkovic on 8.2.2017..
+ */
+export class Product {
+    constructor(
+        public id?: number,
+        public name?: string,
+        public category?: string,
+        public description?: string,
+        public price?: number
+    ) {}
+}
