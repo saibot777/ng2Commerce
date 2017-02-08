a3f1ba04de359f43284302bdeed1dbd8b39df7d9
saibot777 <stefantrajkovic777@gmail.com>
2017-02-08 22:10:15 +0100

    initial commit

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from "@angular/core";
+
+@Component({
+    selector: "app",
+    template: `
+        <div class="bg-success p-a-1 text-xs-center">
+            This is SportsStore
+        </div>
+    `
+})
+export class AppComponent { }
